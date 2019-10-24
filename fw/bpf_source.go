4e58f9f63502ae144a7d45a22e37c84e93069ed6
Dzhemshit Maratov <dzhemshit.maratov@itechart-group.com>
2019-10-24 11:18:31 +0300

    modified bpf_source to potentially filter traffic based on subnets

--- fw/bpf_source.go
+++ fw/bpf_source.go
@@ -3,46 +3,202 @@ package main
 const Source string = `
 #include <uapi/linux/bpf.h>
 #include <linux/ip.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_arp.h>
+#include <linux/if_vlan.h>
+#include <uapi/linux/tcp.h>
+
+// struct ipNet {
+// 	__be32 ip;
+// 	__be32 mask;
+// }
+
+BPF_HASH4(subnets, __be32, ipNet, 100000);
+BPF_HASH4(ipAddrs, __be32, bool, 100000);
+BPF_HASH4(sPorts, __be16, bool, 65600);
+
+
+unsigned char turnOffLeastSignificant(unsigned char b) {
+	bb = b;
+	if bb%2 == 1 {
+		return b - 1;
+	}
+	bb /= 2;
+	if b%2 == 1 {
+		return b - 2;
+	}
+	bb /= 2;
+	if b%2 == 1 {
+		return b - 4;
+	}
+	bb /= 2;
+	if b%2 == 1 {
+		return b - 8;
+	}
+	bb /= 2;
+	if bb%2 == 1 {
+		return b - 16;
+	}
+	bb /= 2;
+	if bb%2 == 1 {
+		return b - 32;
+	}
+	bb /= 2;
+	if bb%2 == 1 {
+		return b - 64;
+	}
+	bb /= 2;
+	if bb%2 == 1 {
+		return b - 128;
+	}
+}
+
+bool isBlacklisted(__be32 *saddr) {
+	bool *p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	// unsigned char *b = (unsigned char*) saddr;
+	// b += 3;
+	// bb = turnOffLeastSignificant(*b);
+	// *b = bb;
+	turnOffLSFromAddr(saddr, 3);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 3);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 3);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 3);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 2);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 2);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 2);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 2);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 1);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 1);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 1);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	turnOffLSFromAddr(saddr, 1);
+	p = subnets.lookup(saddr);
+	if (p) {
+		return true;
+	}
+	return false;
+}
+
+void turnOffLSFromAddr(__be32 *saddr, int offset) {
+	unsigned char *b = (unsigned char*) saddr;
+	b += offset;
+	bb = turnOffLeastSignificant(*b);
+	*b = bb;
+}
 
 int filter(struct xdp_md *meta) {
-	void *data_end = (void*)(long)meta->data_end;
-	void *data = (void*)(long)meta->data;
+	void* data_end = (void*)(long)meta->data_end;
+    void* data = (void*)(long)meta->data;
 
-	struct ethhdr *eth = data;
+    struct ethhdr *eth = data;
 
-	uint16_t h_proto;
-	uint64_t nh_off = 0;
-	int index = 0;
+    // drop packets
+   // let pass XDP_PASS or redirect to tx via XDP_TX
+    long *value;
+    uint16_t h_proto;
+    uint64_t nh_off = 0;
+    int index;
 
-	nh_off = sizeof(*eth);
+    nh_off = sizeof(*eth);
 
-	if (data + nh_off > data_end) {
-		return XDP_DROP;
-	}
+    if (data + nh_off  > data_end)
+        return XDP_DROP;
 
-	h_proto = eth->h_proto;
-	
-	// While the following code appears to be duplicated accidentally,
-	// it's intentional to handle double tags in ethernet frames.
-	if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
-		struct vlan_hdr *vhdr;
-	
-		vhdr = data + nh_off;
-		nh_off += sizeof(struct vlan_hdr);
-		if (data + nh_off > data_end) {
-			return XDP_DROP;
-		}
-		h_proto = vhdr->h_vlan_encapsulated_proto;
-	}
-	if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
-		struct vlan_hdr *vhdr;
-	
-		vhdr = data + nh_off;
-		nh_off += sizeof(struct vlan_hdr);
-		if (data + nh_off > data_end) {
-			return XDP_DROP;
-		}
-		h_proto = vhdr->h_vlan_encapsulated_proto;
-	}
+    h_proto = eth->h_proto;
+
+    // While the following code appears to be duplicated accidentally,
+    // it's intentional to handle double tags in ethernet frames.
+    if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
+        struct vlan_hdr *vhdr;
+
+        vhdr = data + nh_off;
+        nh_off += sizeof(struct vlan_hdr);
+        if (data + nh_off > data_end)
+            return XDP_DROP;
+        h_proto = vhdr->h_vlan_encapsulated_proto;
+    }
+    if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)) {
+        struct vlan_hdr *vhdr;
+
+        vhdr = data + nh_off;
+        nh_off += sizeof(struct vlan_hdr);
+        if (data + nh_off > data_end)
+            return XDP_DROP;
+        h_proto = vhdr->h_vlan_encapsulated_proto;
+    }
+
+
+	// if (h_proto == htons(ETH_P_IP)) {
+	// 	// struct iphdr *iph = data + nh_off;
+	// 	// if ((void*)&iph[1] > data_end) {
+	// 	// 	return XDP_DROP;
+	// 	// }
+	// 	// //char *a = ipAddrs.lookup(&iph->saddr);
+	// 	// //if (a) {
+	// 	// //	return XDP_DROP;
+	// 	// //}
+	// 	// if (iph->saddr != 33) {
+	// 	// 	return XDP_DROP;
+	// 	// }
+	// 	// ///////Did something here with ip headers///////////
+	// 	// nh_off += sizeof(*iph);
+	// 	// struct tcphdr *tcph = data + nh_off;
+	// 	// if ((void*)&tcph[1] > data_end) {
+	// 	// 	return XDP_DROP;
+	// 	// }
+	// 	// char *p = sPorts.lookup(&tcph->source);
+	// 	// if (p) {
+	// 	// 	return XDP_DROP;
+	// 	// }
+	// 	return XDP_DROP;
+	// }
+	return XDP_DROP;
 }
 `
--- fw/fw.go
+++ fw/fw.go
@@ -1,5 +1,119 @@
 package main
 
+/*
+#cgo CFLAGS: -I/usr/include/bcc/compat
+#cgo LDFLAGS: -lbcc
+#include <bcc/bcc_common.h>
+#include <bcc/libbpf.h>
+void perf_reader_free(void *ptr);
+*/
+import "C"
+import (
+	"flag"
+	"log"
+	// "net"
+	"encoding/binary"
+	"os"
+
+	bpf "github.com/iovisor/gobpf/bcc"
+	"github.com/sirupsen/logrus"
+)
+
+func convertToBytes(n int) []byte {
+	buf := make([]byte, 2)
+	binary.LittleEndian.PutUint16(buf, uint16(n))
+	return buf
+}
+
 func main() {
+	ifname := flag.String("i", "", "the name of the network interface")
+	ipAddr := flag.String("s", "", "specify source ip address")
+	rm := flag.Bool("rm", false, "remove filter from network interface, interface name must be specified")
+	port := flag.Int("p", 0, "specify source port")
+	flag.Parse()
+	if *rm {
+		if *ifname == "" {
+			flag.Usage()
+			os.Exit(1)
+		}
+		m := bpf.Module{}
+		err := m.RemoveXDP(*ifname)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		return
+	}
+	if *ifname == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *ipAddr == "" && *port == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	m := bpf.NewModule(Source, []string{
+		"-w",
+	})
+	if m == nil {
+		log.Fatal("failed to initialize bpf module")
+	}
+	defer m.Close()
+	fd, err := m.Load("filter", C.BPF_PROG_TYPE_XDP, 1, 65536)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = m.AttachXDP(*ifname, fd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// This thing makes an error
+	// defer m.RemoveXDP(*ifname)
+
+	// ipAddrs := bpf.NewTable(m.TableId("ipAddrs"), m)
+	// sPorts := bpf.NewTable(m.TableId("sPorts"), m)
+	// if *ipAddr != "" {
+	// 	ip := net.ParseIP(*ipAddr).To4()
+	// 	if len(ip) == 0 {
+	// 		log.Fatal("invalid ip address specified")
+	// 	}
+	// 	err = ipAddrs.Set(ip[:], []byte{0})
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+
+	// err = sPorts.Set(convertToBytes(*port), []byte{0})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// if len(os.Args) < 2 {
+	// 	log.Fatal("cidr mask is expected as an argument")
+	// }
+	// ip, ipNet, err := net.ParseCIDR(os.Args[1])
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// logrus.Infoln(ip, ipNet)
+	// logrus.Infoln("ip len: ", len(ip))
+	// for _, b := range ip {
+	// 	fmt.Printf("%v ", b)
+	// }
+	// fmt.Println()
+	// logrus.Infoln("ipNet ip len: ", len(ipNet.IP))
+	// for _, b := range ipNet.IP {
+	// 	fmt.Printf("%v ", b)
+	// }
+	// fmt.Println()
+	// logrus.Infoln("ipNet mask len: ", len(ipNet.Mask))
+	// for _, b := range ipNet.Mask {
+	// 	fmt.Printf("%v ", b)
+	// }
+	// fmt.Println()
+	// fmt.Printf("%b.%b.%b.%b\n", 192, 168, 0, 0)
+	// i := int(ipNet.Mask[len(ipNet.Mask)-1]) << 1
+	// i := 255
+	// i = i << 1
+	// logrus.Infoln(i)
 
 }
