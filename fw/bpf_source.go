4d0adf4c20ed526366f2500037d164168e2642de
Dzhemshit Maratov <dzhemshit.maratov@itechart-group.com>
2019-10-28 10:34:15 +0300

    modified fw

--- fw/bpf_source.go
+++ fw/bpf_source.go
@@ -14,124 +14,90 @@ const Source string = `
 // 	__be32 mask;
 // }
 
-BPF_HASH4(subnets, __be32, ipNet, 100000);
-BPF_HASH4(ipAddrs, __be32, bool, 100000);
+BPF_HASH4(ipv4, __be32, unsigned char, 100000);
+// BPF_HASH4(addrs, __be32, long long int, 100000);
+// BPF_HASH4(ipAddrs, __be32, bool, 100000);
 BPF_HASH4(sPorts, __be16, bool, 65600);
 
+BPF_PERF_OUTPUT(trace);
 
-unsigned char turnOffLeastSignificant(unsigned char b) {
-	bb = b;
-	if bb%2 == 1 {
-		return b - 1;
-	}
-	bb /= 2;
-	if b%2 == 1 {
-		return b - 2;
-	}
-	bb /= 2;
-	if b%2 == 1 {
-		return b - 4;
-	}
-	bb /= 2;
-	if b%2 == 1 {
-		return b - 8;
-	}
-	bb /= 2;
-	if bb%2 == 1 {
-		return b - 16;
-	}
-	bb /= 2;
-	if bb%2 == 1 {
-		return b - 32;
-	}
-	bb /= 2;
-	if bb%2 == 1 {
-		return b - 64;
-	}
-	bb /= 2;
-	if bb%2 == 1 {
-		return b - 128;
-	}
-}
-
-bool isBlacklisted(__be32 *saddr) {
-	bool *p = subnets.lookup(saddr);
-	if (p) {
+static inline bool addrMatched(struct iphdr *iph, int byteNum) {
+	__be32 saddr = iph->saddr;
+	// unsigned char *c = (unsigned char*) &saddr;
+	unsigned char *c = &saddr;
+	c = c + byteNum;
+	unsigned char t = *c ^ 0b00000001;
+	*c = *c & t;
+	unsigned char* cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	// unsigned char *b = (unsigned char*) saddr;
-	// b += 3;
-	// bb = turnOffLeastSignificant(*b);
-	// *b = bb;
-	turnOffLSFromAddr(saddr, 3);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b00000010;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 3);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b00000100;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 3);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b00001000;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 3);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b00010000;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 2);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b00100000;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 2);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b01000000;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
-	turnOffLSFromAddr(saddr, 2);
-	p = subnets.lookup(saddr);
-	if (p) {
-		return true;
-	}
-	turnOffLSFromAddr(saddr, 2);
-	p = subnets.lookup(saddr);
-	if (p) {
-		return true;
-	}
-	turnOffLSFromAddr(saddr, 1);
-	p = subnets.lookup(saddr);
-	if (p) {
-		return true;
-	}
-	turnOffLSFromAddr(saddr, 1);
-	p = subnets.lookup(saddr);
-	if (p) {
-		return true;
-	}
-	turnOffLSFromAddr(saddr, 1);
-	p = subnets.lookup(saddr);
-	if (p) {
-		return true;
-	}
-	turnOffLSFromAddr(saddr, 1);
-	p = subnets.lookup(saddr);
-	if (p) {
+	t = *c ^ 0b10000000;
+	*c = *c & t;
+	cc = ipv4.lookup(&saddr);
+	if (cc) {
 		return true;
 	}
 	return false;
 }
 
-void turnOffLSFromAddr(__be32 *saddr, int offset) {
-	unsigned char *b = (unsigned char*) saddr;
-	b += offset;
-	bb = turnOffLeastSignificant(*b);
-	*b = bb;
+static bool isFiltered(struct iphdr *iph) {
+	unsigned char *c = ipv4.lookup(&iph->saddr);
+	if (c) {
+		return true;
+	}
+	if (addrMatched(iph, 3)) {
+		return true;
+	}
+	if (addrMatched(iph, 2)) {
+		return true;
+	}
+	if (addrMatched(iph, 1)) {
+		return true;
+	}
+	if (addrMatched(iph, 0)) {
+		return true;
+	}
+	return false;
 }
+
+
 
 int filter(struct xdp_md *meta) {
 	void* data_end = (void*)(long)meta->data_end;
@@ -175,30 +141,30 @@ int filter(struct xdp_md *meta) {
     }
 
 
-	// if (h_proto == htons(ETH_P_IP)) {
-	// 	// struct iphdr *iph = data + nh_off;
-	// 	// if ((void*)&iph[1] > data_end) {
-	// 	// 	return XDP_DROP;
-	// 	// }
-	// 	// //char *a = ipAddrs.lookup(&iph->saddr);
-	// 	// //if (a) {
-	// 	// //	return XDP_DROP;
-	// 	// //}
-	// 	// if (iph->saddr != 33) {
-	// 	// 	return XDP_DROP;
-	// 	// }
-	// 	// ///////Did something here with ip headers///////////
-	// 	// nh_off += sizeof(*iph);
-	// 	// struct tcphdr *tcph = data + nh_off;
-	// 	// if ((void*)&tcph[1] > data_end) {
-	// 	// 	return XDP_DROP;
-	// 	// }
-	// 	// char *p = sPorts.lookup(&tcph->source);
-	// 	// if (p) {
-	// 	// 	return XDP_DROP;
-	// 	// }
-	// 	return XDP_DROP;
-	// }
-	return XDP_DROP;
+	if (h_proto == htons(ETH_P_IP)) {
+		struct iphdr *iph = data + nh_off;
+		if ((void*)&iph[1] > data_end) {
+			return XDP_DROP;
+		}
+		// if (isFiltered(iph)) {
+		// 	 return XDP_DROP;
+		// }
+		unsigned char *c = ipv4.lookup(&iph->saddr);
+		if (c) {
+			return XDP_DROP;
+		} 
+		// ///////Did something here with ip headers///////////
+		// nh_off += sizeof(*iph);
+		// struct tcphdr *tcph = data + nh_off;
+		// if ((void*)&tcph[1] > data_end) {
+		// 	return XDP_DROP;
+		// }
+		// char *p = sPorts.lookup(&tcph->source);
+		// if (p) {
+		// 	return XDP_DROP;
+		// }
+		//return XDP_DROP;
+	}
+	return XDP_PASS;
 }
 `
--- fw/fw.go
+++ fw/fw.go
@@ -9,47 +9,85 @@ void perf_reader_free(void *ptr);
 */
 import "C"
 import (
-	"flag"
+	"encoding/json"
+	"fmt"
 	"log"
-	// "net"
-	"encoding/binary"
+	"net"
 	"os"
 
 	bpf "github.com/iovisor/gobpf/bcc"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
 )
+
+type blackListRaw struct {
+	IPAddrs []string
+	Hosts   []string
+	Ports   []int
+}
+type BlackList struct {
+	IPAddrs []net.IP
+	Ports   []int
+}
+
+func BlackListed() (BlackList, error) {
+	f, err := os.Open("rogue_blacklist.json")
+	if err != nil {
+		return BlackList{}, err
+	}
+	var blr blackListRaw
+	err = json.NewDecoder(f).Decode(&blr)
+	if err != nil {
+		return BlackList{}, err
+	}
+	bl := BlackList{}
+	for _, p := range blr.Ports {
+		if p < 1 || p > 65536 {
+			return BlackList{}, fmt.Errorf("%d port must be between 1 and 65536", p)
+		}
+	}
+	bl.Ports = blr.Ports
+	for _, addr := range blr.IPAddrs {
+		ip := net.ParseIP(addr).To4()
+		if ip == nil {
+			return BlackList{}, fmt.Errorf("%s is an IPv6 address, IPv4 address expected", addr)
+		}
+		bl.IPAddrs = append(bl.IPAddrs, ip)
+	}
+	for _, host := range blr.Hosts {
+		addrs, err := net.LookupHost(host)
+		if err != nil {
+			return BlackList{}, err
+		}
+		fmt.Println(addrs)
+		for _, addr := range addrs {
+			ip := net.ParseIP(addr).To4()
+			if ip != nil {
+				fmt.Println(host, ip)
+				bl.IPAddrs = append(bl.IPAddrs, ip)
+			}
+		}
+	}
+	return bl, nil
+}
 
 func convertToBytes(n int) []byte {
 	buf := make([]byte, 2)
-	binary.LittleEndian.PutUint16(buf, uint16(n))
+	// binary.LittleEndian.PutUint16(buf, uint16(n))
 	return buf
 }
 
-func main() {
-	ifname := flag.String("i", "", "the name of the network interface")
-	ipAddr := flag.String("s", "", "specify source ip address")
-	rm := flag.Bool("rm", false, "remove filter from network interface, interface name must be specified")
-	port := flag.Int("p", 0, "specify source port")
-	flag.Parse()
-	if *rm {
-		if *ifname == "" {
-			flag.Usage()
-			os.Exit(1)
-		}
-		m := bpf.Module{}
-		err := m.RemoveXDP(*ifname)
+func start(c *cli.Context) {
+	if !c.Args().Present() {
+		err := cli.ShowSubcommandHelp(c)
 		if err != nil {
 			logrus.Fatal(err)
 		}
-		return
-	}
-	if *ifname == "" {
-		flag.Usage()
 		os.Exit(1)
 	}
-	if *ipAddr == "" && *port == 0 {
-		flag.Usage()
-		os.Exit(1)
+	blacklist, err := BlackListed()
+	if err != nil {
+		logrus.Fatal(err)
 	}
 	m := bpf.NewModule(Source, []string{
 		"-w",
@@ -58,62 +96,192 @@ func main() {
 		log.Fatal("failed to initialize bpf module")
 	}
 	defer m.Close()
-	fd, err := m.Load("filter", C.BPF_PROG_TYPE_XDP, 1, 65536)
+	fd, err := m.Load("filter", C.BPF_PROG_TYPE_XDP, 1, 200000)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = m.AttachXDP(*ifname, fd)
+	err = m.AttachXDP(c.Args().First(), fd)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// This thing makes an error
-	// defer m.RemoveXDP(*ifname)
-
-	// ipAddrs := bpf.NewTable(m.TableId("ipAddrs"), m)
-	// sPorts := bpf.NewTable(m.TableId("sPorts"), m)
-	// if *ipAddr != "" {
-	// 	ip := net.ParseIP(*ipAddr).To4()
-	// 	if len(ip) == 0 {
-	// 		log.Fatal("invalid ip address specified")
-	// 	}
-	// 	err = ipAddrs.Set(ip[:], []byte{0})
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
-
-	// err = sPorts.Set(convertToBytes(*port), []byte{0})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if len(os.Args) < 2 {
-	// 	log.Fatal("cidr mask is expected as an argument")
-	// }
-	// ip, ipNet, err := net.ParseCIDR(os.Args[1])
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// logrus.Infoln(ip, ipNet)
-	// logrus.Infoln("ip len: ", len(ip))
-	// for _, b := range ip {
-	// 	fmt.Printf("%v ", b)
-	// }
-	// fmt.Println()
-	// logrus.Infoln("ipNet ip len: ", len(ipNet.IP))
-	// for _, b := range ipNet.IP {
-	// 	fmt.Printf("%v ", b)
-	// }
-	// fmt.Println()
-	// logrus.Infoln("ipNet mask len: ", len(ipNet.Mask))
-	// for _, b := range ipNet.Mask {
-	// 	fmt.Printf("%v ", b)
-	// }
-	// fmt.Println()
-	// fmt.Printf("%b.%b.%b.%b\n", 192, 168, 0, 0)
-	// i := int(ipNet.Mask[len(ipNet.Mask)-1]) << 1
-	// i := 255
-	// i = i << 1
-	// logrus.Infoln(i)
-
+	subnets := bpf.NewTable(m.TableId("ipv4"), m)
+	for _, ip := range blacklist.IPAddrs {
+		err := subnets.Set(ip[:], []byte{1})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
+
+func stop(c *cli.Context) {
+	if !c.Args().Present() {
+		err := cli.ShowSubcommandHelp(c)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		os.Exit(1)
+	}
+	m := bpf.Module{}
+	err := m.RemoveXDP(c.Args().First())
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+//
+// func drop(c *cli.Context) {
+// 	if !c.Args().Present() {
+// 		err := cli.ShowSubcommandHelp(c)
+// 		if err != nil {
+// 			logrus.Fatal(err)
+// 		}
+// 		os.Exit(1)
+// 	}
+// 	// m := bpf.Module{}
+// 	m := bpf.NewModule(Source, []string{
+// 		"-w",
+// 	})
+// 	if m == nil {
+// 		log.Fatal("failed to initialize bpf module")
+// 	}
+// 	defer m.Close()
+// 	fd, err := m.Load("filter", C.BPF_PROG_TYPE_XDP, 1, 200000)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	err = m.AttachXDP(c.Args().First(), fd)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	subnets := bpf.NewTable(m.TableId("subnets"), m)
+// 	// sPorts := bpf.NewTable(m.TableId("sPorts"), m)
+// 	saddr := c.String("saddr")
+// 	if saddr != "" {
+// 		ip := net.ParseIP(saddr).To4()
+// 		if len(ip) == 0 {
+// 			log.Fatal("invalid ip address specified")
+// 		}
+// 		err := subnets.Set(ip[:], []byte{1})
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// }
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "rogue"
+	app.Author = "Jem Maratov"
+	app.Version = "1.0.0"
+	app.Commands = []cli.Command{
+		{
+			Name:   "start",
+			Usage:  "starts filtering incoming traffic for the specified network interface",
+			Action: start,
+			// Flags: []cli.Flag{
+			// 	cli.StringFlag{
+			// 		Name:  "saddr",
+			// 		Usage: "source ip address",
+			// 	},
+			// },
+		},
+		{
+			Name:   "stop",
+			Usage:  "stops filtering incoming traffic for the specified network interface",
+			Action: stop,
+		},
+	}
+	app.Run(os.Args)
+}
+
+// func main() {
+// 	ifname := flag.String("i", "", "the name of the network interface")
+// 	ipAddr := flag.String("s", "", "specify source ip address")
+// 	rm := flag.Bool("rm", false, "remove filter from network interface, interface name must be specified")
+// 	port := flag.Int("p", 0, "specify source port")
+// 	flag.Parse()
+// 	if *rm {
+// 		if *ifname == "" {
+// 			flag.Usage()
+// 			os.Exit(1)
+// 		}
+// 		m := bpf.Module{}
+// 		err := m.RemoveXDP(*ifname)
+// 		if err != nil {
+// 			logrus.Fatal(err)
+// 		}
+// 		return
+// 	}
+// 	if *ifname == "" {
+// 		flag.Usage()
+// 		os.Exit(1)
+// 	}
+// 	if *ipAddr == "" && *port == 0 {
+// 		flag.Usage()
+// 		os.Exit(1)
+// 	}
+// 	m := bpf.NewModule(Source, []string{
+// 		"-w",
+// 	})
+// 	if m == nil {
+// 		log.Fatal("failed to initialize bpf module")
+// 	}
+// 	defer m.Close()
+// 	fd, err := m.Load("filter", C.BPF_PROG_TYPE_XDP, 1, 65536)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	err = m.AttachXDP(*ifname, fd)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	// This thing makes an error
+// 	// defer m.RemoveXDP(*ifname)
+//
+// 	subnets := bpf.NewTable(m.TableId("subnets"), m)
+// 	// sPorts := bpf.NewTable(m.TableId("sPorts"), m)
+// 	if *ipAddr != "" {
+// 		ip := net.ParseIP(*ipAddr).To4()
+// 		if len(ip) == 0 {
+// 			log.Fatal("invalid ip address specified")
+// 		}
+// 		err = subnets.Set(ip[:], []byte{23})
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// 	// err = sPorts.Set(convertToBytes(*port), []byte{0})
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+//
+// 	// if len(os.Args) < 2 {
+// 	// 	log.Fatal("cidr mask is expected as an argument")
+// 	// }
+// 	// ip, ipNet, err := net.ParseCIDR(os.Args[1])
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	// logrus.Infoln(ip, ipNet)
+// 	// logrus.Infoln("ip len: ", len(ip))
+// 	// for _, b := range ip {
+// 	// 	fmt.Printf("%v ", b)
+// 	// }
+// 	// fmt.Println()
+// 	// logrus.Infoln("ipNet ip len: ", len(ipNet.IP))
+// 	// for _, b := range ipNet.IP {
+// 	// 	fmt.Printf("%v ", b)
+// 	// }
+// 	// fmt.Println()
+// 	// logrus.Infoln("ipNet mask len: ", len(ipNet.Mask))
+// 	// for _, b := range ipNet.Mask {
+// 	// 	fmt.Printf("%v ", b)
+// 	// }
+// 	// fmt.Println()
+// 	// fmt.Printf("%b.%b.%b.%b\n", 192, 168, 0, 0)
+// 	// i := int(ipNet.Mask[len(ipNet.Mask)-1]) << 1
+// 	// i := 255
+// 	// i = i << 1
+// 	// logrus.Infoln(i)
+//
+// }
